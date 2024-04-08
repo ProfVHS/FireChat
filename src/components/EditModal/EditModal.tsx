2fe8289241a7ab4c0548b8ea60ad91770927914f
ProfVHS <rubajrafal04@gmail.com>
2024-04-08 18:24:29 +0200

    fixed deleting user and added deleting messages connected with deleted user

--- src/components/EditModal/EditModal.tsx
+++ src/components/EditModal/EditModal.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./style.scss";
 import { updateProfile } from "firebase/auth";
 import { auth, firestore } from "../../App";
-import { collection, deleteDoc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, getDocs, or, query, updateDoc, where } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
 interface EditModalProps {
@@ -34,20 +34,34 @@ export const EditModal = ({ setEditModal }: EditModalProps) => {
     setEditModal(false);
   };
 
-  const deleteAccount = async () => {
+  const deleteAllMessages = async (uid: string) => {
+    const q = query(collection(firestore, "messages"), or(where("uid", "==", uid), where("sentTo.uid", "==", uid)));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach(async (doc) => {
+      await deleteDoc(doc.ref);
+    });
+  };
+
+  const deleteAccount = async (uid: string) => {
+    const q = query(collection(firestore, "users"), where("uid", "==", uid));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      const docRef = querySnapshot.docs[0];
+      await deleteDoc(docRef.ref);
+    }
+    auth.currentUser?.delete();
+  };
+
+  const handleDeleteButton = async () => {
     const user = auth.currentUser;
     if (user) {
-      const q = query(collection(firestore, "users"), where("uid", "==", user.uid));
-      const querySnapshot = await getDocs(q);
-
-      if (!querySnapshot.empty) {
-        const docRef = querySnapshot.docs[0];
-        await deleteDoc(docRef.ref);
-      }
-      auth.currentUser?.delete();
-      auth.signOut();
-      navigate("/");
+      await deleteAllMessages(user.uid);
+      await deleteAccount(user.uid);
     }
+    auth.signOut();
+    navigate("/");
   };
 
   return (
@@ -64,7 +78,7 @@ export const EditModal = ({ setEditModal }: EditModalProps) => {
           <button className="modal__content__button cancel" onClick={() => setEditModal(false)}>
             Cancel
           </button>
-          <button className="modal__content__button delete" onClick={deleteAccount}>
+          <button className="modal__content__button delete" onClick={handleDeleteButton}>
             Delete Account
           </button>
         </div>
